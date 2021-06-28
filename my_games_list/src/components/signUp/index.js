e8c1a1d8d494c337841149789238d8bc1fbfa549
CiaranCrowley <ciarancrowley37@gmail.com>
2021-06-28 18:37:44 +0100

    Created Context file.  Strange Firebase error saying API Key is invalid??????

--- my_games_list/src/components/signUp/index.js
+++ my_games_list/src/components/signUp/index.js
@@ -1,19 +1,45 @@
+/* eslint-disable no-unused-vars */
 // @ts-nocheck
-import React, { useRef } from 'react'
-import { Button, Card, Form } from 'react-bootstrap'
+import React, { useRef, useState } from 'react'
+import { Button, Card, Form, Alert } from 'react-bootstrap'
+import { useAuth } from '../../contexts/AuthContext'
+import { Link, useHistory } from 'react-router-dom'
 
 export default function Signup() {
 	const emailRef = useRef()
 	const passwordRef = useRef()
 	const passwordConfirmRef = useRef()
+	const { signup } = useAuth()
+	const [error, setError] = useState('')
+	const [loading, setLoading] = useState(false)
+	const history = useHistory()
+
+	async function handleSubmit(e) {
+		e.preventDefault()
+
+		if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+			return setError('Passwords do not match')
+		}
+
+		try {
+			setError('')
+			setLoading(true)
+			await signup(emailRef.current.value, passwordRef.current.value)
+			history.push('/')
+		} catch {
+			setError('Failed to create an account')
+		}
+
+		setLoading(false)
+	}
 
 	return (
 		<>
 			<Card>
 				<Card.Body>
 					<h2 className="text-center mb-4">Sign Up</h2>
-					{/* {error && <Alert variant="danger">{error}</Alert>} */}
-					<Form>
+					{error && <Alert variant="danger">{error}</Alert>}
+					<Form onSubmit={handleSubmit}>
 						<Form.Group id="email">
 							<Form.Label>Email</Form.Label>
 							<Form.Control type="email" ref={emailRef} required />
@@ -30,7 +56,7 @@ export default function Signup() {
 								required
 							/>
 						</Form.Group>
-						<Button className="w-100" type="submit">
+						<Button disabled={loading} className="w-100" type="submit">
 							Sign Up
 						</Button>
 					</Form>
