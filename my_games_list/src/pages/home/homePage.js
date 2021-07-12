3403fe69a9c3bcd41265345941d7786f3be703fe
CiaranCrowley <ciarancrowley37@gmail.com>
2021-07-12 17:12:37 +0100

    Made a start on the home page

--- my_games_list/src/pages/home/homePage.js
+++ my_games_list/src/pages/home/homePage.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import SiteHeader from '../../components/siteHeader/siteHeader'
+import { Container, Card } from 'react-bootstrap'
 
 export default function Home() {
 	/**
+	 * ?  Try using a container first.  If that doesn't allow the list to be populated, remove it and resize the cards
+	 * !  Above line relies on games context being functional
 	 * ?  If user is not logged in, don't allow them to add games to lists, else, allow list expansion
 	 */
 
@@ -10,6 +13,9 @@ export default function Home() {
 		<div>
 			<SiteHeader />
 			HOME PAGE
+			<Container>
+				<Card>HELLO</Card>
+			</Container>
 		</div>
 	)
 }
